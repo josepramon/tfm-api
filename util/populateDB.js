a9a26a9fa4391712e8a6852b9bffc9736e99eb0b
josep ramon <jrcamps@gmail.com>
2015-10-20 22:02:41 +0200

    Use the new mongo config retrieval method

--- util/populateDB.js
+++ util/populateDB.js
@@ -14,11 +14,11 @@ var
 var mongoConfigParser = require('../src/lib/mongoConfigParser');
 
 var mongoConn = new mongoConfigParser().setEnv({
-  host     : process.env.MONGO_HOST,
-  port     : process.env.MONGO_PORT,
-  user     : process.env.MONGO_USER,
-  password : process.env.MONGO_PASSWORD,
-  database : process.env.MONGO_DATABASE
+  host     : config.mongo.host,
+  port     : config.mongo.port,
+  user     : config.mongo.user,
+  password : config.mongo.password,
+  database : config.mongo.database
 });
 
 // DATA LOAD:
