214ff17f0b1dcf0bc68425bd9f82534c35ebbf1b
josep ramon <jrcamps@gmail.com>
2015-10-26 19:16:56 +0100

    Add bad request error

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -7,6 +7,7 @@ module.exports = {
   App :         Error.extend('AppError',          500),
   NotFound:     Error.extend('HttpNotFoundError', 404),
   Unauthorized: Error.extend('HttpUnauthorized',  401),
+  BadRequest:   Error.extend('HttpBadRequest',    400),
 
   Validation : function(model, attribute, message) {
     var err = new MongooseError.ValidationError(model);
