8a2e5b2769d1ff7793c74ee756a814b0b2dfd7c1
josep ramon <jrcamps@gmail.com>
2015-10-26 02:00:59 +0100

    Create a middleware to inject additional body params

---
+++ src/modules/api/middleware/bodyParamsInjector.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var _ = require('lodash');
+
+/**
+ * Inject additional parameters to the req.body
+ *
+ * Warning: This is only for simple parameter injection, not for complex
+ * nested objects, so if the req.body is `{ foo: 1, bar: {baz:1, qux: 2} }`
+ * and the params is `{ bar: {baz:2} }`, the result will be:
+ * `{foo: 1, bar: {baz: 2}}`
+ * An implementation with deep nesting support could be possible, but it's
+ * not needed.
+ *
+ * @param {Object}  params   New parameters to add to the req.body
+ * @param {Boolean} override If true, the new parameters will override
+ *                           any preexisting key with the sam name
+ */
+var injectParams = function(params, override, req, res, next) {
+  req.body = req.body || {};
+
+  if(override) {
+    _.extend(req.body, params);
+  } else {
+    _.defaults(req.body, params);
+  }
+
+  next();
+};
+
+
+module.exports = {
+  middleware: injectParams,
+};
---
+++ test/unit/modules/api/middleware/bodyParamsInjector.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var
+  async          = require('async'),
+  // test dependencies
+  mocha          = require('mocha'),
+  expect         = require('chai').expect,
+  requireHelper  = require('test/_util/require_helper'),
+
+  // file to test
+  injectParams = requireHelper('modules/api/middleware/bodyParamsInjector').middleware;
+
+
+describe('bodyParamsInjector', function() {
+
+  it('should add some additional parameters if they don\'t exist', function(done) {
+
+    var req = {}, params, override = false;
+
+    async.series([
+      function(cb) {
+        req.body = null;
+        params   = {foo: 1};
+
+        injectParams(params, override, req, null, function() {
+          expect(req.body).to.be.an('object');
+          expect(req.body).to.deep.equal(params);
+          cb();
+        });
+      },
+
+      function(cb) {
+        req.body = {foo: 2, bar: 1};
+        params   = {foo: 1, qux: 2};
+
+        injectParams(params, override, req, null, function() {
+          expect(req.body).to.be.an('object');
+          expect(req.body).to.deep.equal({foo: 2, bar: 1, qux: 2});
+          cb();
+        });
+        cb();
+      }
+    ], function(err) {
+      done();
+    });
+
+  });
+
+
+  it('should replace some additional parameters if they exist', function(done) {
+
+    var req = {}, params, override = true;
+
+    async.series([
+      function(cb) {
+        req.body = null;
+        params   = {foo: 1};
+
+        injectParams(params, override, req, null, function() {
+          expect(req.body).to.be.an('object');
+          expect(req.body).to.deep.equal(params);
+          cb();
+        });
+      },
+
+      function(cb) {
+        req.body = {foo: 2, bar: 1};
+        params   = {foo: 1, qux: 2};
+
+        injectParams(params, override, req, null, function() {
+          expect(req.body).to.be.an('object');
+          expect(req.body).to.deep.equal({foo: 1, bar: 1, qux: 2});
+          cb();
+        });
+        cb();
+      }
+    ], function(err) {
+      done();
+    });
+
+  });
+
+});
