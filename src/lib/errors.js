73957470506f1f5516eda1a579f219a3b29ca9a8
josep ramon <jrcamps@gmail.com>
2015-11-04 16:28:15 +0100

    Create custom validation error for 'unique' constrains

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -9,6 +9,9 @@ module.exports = {
   Unauthorized: Error.extend('HttpUnauthorized',  401),
   BadRequest:   Error.extend('HttpBadRequest',    400),
 
+  /**
+   * Custom validation error
+   */
   Validation : function(model, attribute, message) {
     var err = new MongooseError.ValidationError(model);
     err.errors[attribute] = new MongooseError.ValidatorError({
@@ -17,5 +20,28 @@ module.exports = {
       path: attribute
     });
     return err;
+  },
+
+  /**
+   * Unique constraint error
+   *
+   * Generally this should not be used directly, when attempting to save a model,
+   * the validations are run and the appropiate errors are thrown, but sometimes
+   * it may be necessary to check for some attribute uniqueness and throw an error
+   * without actually saving the model.
+   */
+  UniqueKey: function(model, attribute, message) {
+
+    var
+      db         = model.db.name,
+      collection = model.collection,
+      msg        = 'E11000 duplicate key error index: ' + db + '.' + collection + '.$' + attribute + '_1 dup key';
+
+    var err = new MongooseError(msg);
+
+    err.code = 11000;
+
+    return err;
   }
+
 };
