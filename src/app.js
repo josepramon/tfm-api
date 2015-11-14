1e20289bd6e72d5e37a487d0dc7134b8134d83c6
josep ramon <jrcamps@gmail.com>
2015-11-14 17:48:33 +0100

    Disable cluster mode

--- src/app.js
+++ src/app.js
@@ -108,12 +108,12 @@ var createApp = function() {
 
 
 /* istanbul ignore else */
-if (process.env.DEV) {
+// if (process.env.DEV) {
   // dev/test mode: just create one instance
   exports.app = createApp();
 
-} else {
-  // production mode: clustered, one worker per core
-  var cluster = require('express-cluster');
-  cluster(createApp);
-}
+// } else {
+//   // production mode: clustered, one worker per core
+//   var cluster = require('express-cluster');
+//   cluster(createApp);
+// }
