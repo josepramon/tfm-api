7bcad8e7b7ed8eab4cc4f883b0e37c6649017079
josep ramon <jrcamps@gmail.com>
2015-12-13 00:56:36 +0100

    Add default ticket statuses

---
+++ data/ticketStatuses.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports['tickets.statuses'] = [
+  {
+    name: 'Open',
+    order: 0,
+    open:  true
+  },
+  {
+    name: 'Closed',
+    order: 100,
+    closed:  true
+  }
+];
